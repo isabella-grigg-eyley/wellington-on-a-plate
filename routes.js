b5bb7ae74dd15fddc61ec76cf8cbd3f910d7a0a8
rob-bee-neilson <robbie.neilson@gmail.com>
2018-08-17 11:05:05 +1200

    testing post routes

--- routes.js
+++ routes.js
@@ -3,9 +3,28 @@ const router = express.Router();
 const fs = require('fs');
 
 let data = require('./data.json');
+let burgerList = data["burgers"]
 
 router.get('/', (req, res) => {
-    res.send('burgers mmmm');
+    //render home page with sidebar invisible by default
+    //res.send('burgers mmmm');
+    //res.send(burgerList[0]["name"])
+    res.render('./home')
+})
+
+router.post('/', (req, res) => {
+    console.log("post req received")
+    res.send(req.body);
+    //let burgerID = [req.body["id"]]
+    //burgerID = "1"
+
+    //req.body === {"id": "1"}
+    //get ID of burger clicked,
+    //find burger info in data file
+    //render page with burger info sent to it
+    //let burger =  data["burgers"].find((element) => {
+        //return burgerID == element.id
+    
 })
 
 module.exports = router;
